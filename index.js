93bffb4033958d0cad84294ef7d4b111bfb70c88
sashap8 <sasha.mahalia@produce8.com>
2022-06-27 16:16:36 -0700

    Write new value to param store if one isn't found

--- index.js
+++ index.js
@@ -1,57 +1,65 @@
-const core = require('@actions/core');
-const ssm = require('./ssm-helper');
+const core = require("@actions/core");
+const ssm = require("./ssm-helper");
 
-const execSync = require('child_process').execSync;
+const execSync = require("child_process").execSync;
 
 async function run_action() {
-    const ssmPath = core.getInput('ssm-path', { required: true });
-    const prefix = core.getInput('prefix');
-    const region = process.env.AWS_DEFAULT_REGION;
-    const decryption = core.getInput('decryption') === 'true';
-    const nullable = core.getInput('nullable') === 'true';
+  const ssmPath = core.getInput("ssm-path", { required: true });
+  const ssmValue = core.getInput("ssm-value", { required: false });
+  const ssmType = core.getInput("ssm-value-type", { required: false });
+  const keyName = core.getInput("key-name");
+  const region = process.env.AWS_DEFAULT_REGION;
+  const decryption = core.getInput("decryption") === "true";
+  const nullable = core.getInput("nullable") === "true";
 
-    try {
-
-        paramValue = await ssm.getParameter(ssmPath, decryption, region);
-        parsedValue = parseValue(paramValue);
-        if (typeof(parsedValue) === 'object') {
-            core.debug(`parsedValue: ${JSON.stringify(parsedValue)}`);
-            // Assume basic JSON structure
-            for (var key in parsedValue) {
-                setEnvironmentVar(prefix + key, parsedValue[key])
-            }
-        } else {
-            core.debug(`parsedValue: ${parsedValue}`);
-            // Set environment variable with ssmPath name as the env variable
-            var split = ssmPath.split('/');
-            var envVarName = prefix + split[split.length - 1];
-            core.debug(`Using prefix + end of ssmPath for env var name: ${envVarName}`);
-            setEnvironmentVar(envVarName, parsedValue);
-        }
-        
-    } catch (error) {
-        if(!nullable) {
-            core.setFailed(error.message);
-        } else {
-            core.debug(`could not find parameter: ${error.message}`);
-        }
+  try {
+    const paramValue = await ssm.getParameter(ssmPath, decryption, region);
+    const parsedValue = parseValue(paramValue);
+    if (typeof parsedValue === "object") {
+      core.debug(`parsedValue: ${JSON.stringify(parsedValue)}`);
+      // Assume basic JSON structure
+      for (var key in parsedValue) {
+        setEnvironmentVar(keyName + key, parsedValue[key]);
+      }
+    } else {
+      core.debug(`parsedValue: ${parsedValue}`);
+      // Set environment variable with ssmPath name as the env variable
+      const split = ssmPath.split("/");
+      const envVarName = keyName + split[split.length - 1];
+      core.debug(
+        `Using keyName + end of ssmPath for env var name: ${envVarName}`
+      );
+      setEnvironmentVar(envVarName, parsedValue);
     }
+  } catch (error) {
+    if (error instanceof ParameterNotFound) {
+      core.debug(`could not find parameter, attemping to create parameter`);
+      createSsmValue(ssmPath, region, ssmValue, ssmType);
+      run_action(ssmPath, decryption, region);
+    }
+    if (!nullable) {
+      core.setFailed(error.message);
+    } else {
+      core.debug(`could not find parameter: ${error.message}`);
+    }
+  }
 }
 
-
 function parseValue(val) {
-    try {
-        return JSON.parse(val);
-    } catch (error) {
-        core.debug('JSON parse failed - assuming parameter is to be taken as a string literal');
-        return val;
-    }
+  try {
+    return JSON.parse(val);
+  } catch (error) {
+    core.debug(
+      "JSON parse failed - assuming parameter is to be taken as a string literal"
+    );
+    return val;
+  }
 }
 
 function setEnvironmentVar(key, value) {
-    cmdString = `echo "${key}=${value}" >> $GITHUB_ENV`;
-    core.debug(`Running cmd: ${cmdString}`);
-    execSync(cmdString, {stdio: 'inherit'});
+  cmdString = `echo "${key}=${value}" >> $GITHUB_ENV`;
+  core.debug(`Running cmd: ${cmdString}`);
+  execSync(cmdString, { stdio: "inherit" });
 }
 
 run_action();
--- ssm-helper.js
+++ ssm-helper.js
@@ -1,15 +1,42 @@
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 
 const getParameter = async (ssmPath, decryption, region) => {
-    AWS.config.update({region: region});
-    const ssm = new AWS.SSM();
-    const params =
-    {
-        Name: ssmPath,
-        WithDecryption: decryption
-    };
-    const result = await ssm.getParameter(params).promise();
-    return result.Parameter.Value;
-}
+  AWS.config.update({ region: region });
+  const ssm = new AWS.SSM();
+  const params = {
+    Name: ssmPath,
+    WithDecryption: decryption,
+  };
+  const result = await ssm.getParameter(params).promise();
+  return result.Parameter.Value;
+};
 
-module.exports = {getParameter};
+const createSsmValue = async (ssmPath, region, ssmValue, ssmType) => {
+  try {
+    core.info(`Storing Variable in path [${ssmPath}]`);
+    // Load the AWS Region to use in SSM
+    core.debug(`Setting aws-region [${region}]`);
+    AWS.config.update({ region: region });
+    const ssm = new AWS.SSM();
+    const params = {
+      Name: ssmPath,
+      Value: ssmValue,
+      Type: ssmType,
+      Overwrite: false,
+    };
+    const keyId = core.getInput("ssm-kms-key-id");
+    if (params["Type"] === "SecureString" && keyId !== "") {
+      core.debug(`Setting the KeyId to ${keyId}`);
+      params["KeyId"] = keyId;
+    }
+    const result = await ssm.putParameter(params);
+    core.debug(
+      `Parameter details Version [${result.Version}] Tier [${result.Tier}]`
+    );
+    core.info(`Successfully Stored parameter in path [${ssmPath}]`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
+
+module.exports = { getParameter, createSsmValue };
